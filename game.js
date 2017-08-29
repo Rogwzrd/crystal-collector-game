0e0520557ee9a15d9dad65730f011a97eb6633bc
unknown <mgdaye@gmail.com>
2017-08-28 20:13:49 -0400

    added variance to the onclick functions for gems

--- game.js
+++ game.js
@@ -1,46 +1,56 @@
+var wins = 0;
+var losses = 0;
+var targetScore = Math.floor(Math.random() * 75) + 25;
+var currentScore = 0;
+var gems = {
+    redGem: Math.floor(Math.random() * 13) + 1,
+    refreshRedGem: true,
+    blueGem: Math.floor(Math.random() * 13) + 1,
+    refreshBlueGem: true,
+    greenGem: Math.floor(Math.random() * 13) + 1,
+    refreshGreenGem: true,
+    purpleGem: Math.floor(Math.random() * 13) + 1,
+    refreshPurpleGem: true
+};
 
-
-    var wins = 0;
-    var losses = 0;
-    var targetScore = 0;
-    var currentScore = 0;
-    var gems = {
-        redGem: 0,
-        refreshRedGem: true,
-        blueGem: 1,
-        refreshBlueGem: true,
-        greenGem: 2,
-        refreshGreenGem: true,
-        purpleGem: 3,
-        refreshPurpleGem: true
-    };
+function gameStart() {
+	targetScore = Math.floor(Math.random() * 75) + 25;
+	currentScore = 0;
+}
 
 $(document).ready(function() {
 
-//////////////////////////////////////////////
-// global variables
-//////////////////////////////////////////////
-
-
-
-
-
+    //////////////////////////////////////////////
+    // global variables
+    //////////////////////////////////////////////
 
     $(".red-gem").on("click", function() {
-        gems.redGem++;
-        console.log(gems.redGem)
+        if (gems.refreshRedGem == true) {
+            gems.redGem = Math.floor(Math.random() * 13) + 1;
+            gems.refreshRedGem = false;
+            console.log(gems.redGem)
+        } else if (targetScore > currentScore) {
+        	console.log(gems.redGem);
+        	currentScore = currentScore + gems.redGem;
+        	console.log("my target score is: " + targetScore);
+        	console.log("my current score is: " + currentScore)
+        } else if (targetScore < currentScore) {
+        	losses++;
+        	gameStart();
+      		alert("Oh shucks! My pickaxe broke!")
+        }
     });
 
     $(".blue-gem").on("click", function() {
-        gems.blueGem++;
+        gems.blueGem = Math.floor(Math.random() * 12) + 1;
         console.log(gems.blueGem)
     });
     $(".green-gem").on("click", function() {
-        gems.greenGem++;
+        gems.greenGem = Math.floor(Math.random() * 12) + 1;
         console.log(gems.greenGem)
     });
-    $(".purple-gem").on("clic", function() {
-        gems.purpleGem++;
+    $(".purple-gem").on("click", function() {
+        gems.purpleGem = Math.floor(Math.random() * 12) + 1;
         console.log(gems.purpleGem)
     });
 
