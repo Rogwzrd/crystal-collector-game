bba539bdf365ee023d96a1b60a752e53351f4585
unknown <mgdaye@gmail.com>
2017-08-28 21:58:46 -0400

    wet code in need of refactoring

--- game.js
+++ game.js
@@ -11,25 +11,47 @@ var gems = {
     blueGem: Math.floor(Math.random() * 13) + 1,
     greenGem: Math.floor(Math.random() * 13) + 1,
     purpleGem: Math.floor(Math.random() * 13) + 1,
-    refreshGem: true
+    refreshRedGem: true,
+    refreshBlueGem: true,
+    refreshGreenGem: true,
+    refreshPurpleGem: true
 };
+
+function gameState() {
+    //this line determines whether a game is won or lost on click
+    if (targetScore < currentScore) {
+        losses++;
+        gameStart();
+        alert("Oh shucks! My pickaxe broke!")
+
+    }
+    //this line runs if your target score and the current score match
+    if (targetScore === currentScore) {
+        wins++;
+        gameStart();
+        alert("We're gonna be rich!")
+    }
+}
 
 function gameStart() {
     targetScore = Math.floor(Math.random() * 75) + 25;
     currentScore = 0;
-    gems.refreshGem = true;
+    gems.refreshRedGem = true;
+    gems.refreshBlueGem = true;
+    gems.refreshGreenGem = true;
+    gems.refreshPurpleGem = true;
     console.log('Lookie here, fresh gems!')
 }
 
 $(document).ready(function() {
 
-	//this line determines whether or not the value of the gems change
+    //this line determines whether or not the value of the gems change
     $(".red-gem").on("click", function() {
-        if (gems.refreshGem == true) {
+        if (gems.refreshRedGem === true) {
             gems.redGem = Math.floor(Math.random() * 13) + 1;
-            gems.refreshGem = false;
+            gems.refreshRedGem = false;
             console.log(gems.redGem)
-        } 
+        }
 
         //this line runs if your score is below the target score
         if (targetScore > currentScore) {
@@ -38,34 +60,60 @@ $(document).ready(function() {
             console.log("my target score is: " + targetScore);
             console.log("my current score is: " + currentScore)
         }
-
-        //this line determines whether a game is won or lost on click
-        if (targetScore < currentScore) {
-            losses++;
-            gameStart();
-            alert("Oh shucks! My pickaxe broke!")
-
-        }
-        //this line runs if your target score and the current score match
-        if (targetScore === currentScore) {
-            wins++;
-            gameStart();
-            alert("We're gonna be rich!")
-        }
-        console.log(gems.refreshGem)
+        gameState();
     });
 
     $(".blue-gem").on("click", function() {
-        gems.blueGem = Math.floor(Math.random() * 12) + 1;
-        console.log(gems.blueGem)
+        if (gems.refreshBlueGem === true) {
+            gems.blueGem = Math.floor(Math.random() * 13) + 1;
+            gems.refreshBlueGem = false;
+            console.log(gems.blueGem)
+        }
+
+        //this line runs if your score is below the target score
+        if (targetScore > currentScore) {
+            console.log(gems.blueGem);
+            currentScore = currentScore + gems.blueGem;
+            console.log("my target score is: " + targetScore);
+            console.log("my current score is: " + currentScore)
+        }
+        gameState();
     });
+
     $(".green-gem").on("click", function() {
-        gems.greenGem = Math.floor(Math.random() * 12) + 1;
-        console.log(gems.greenGem)
+        if (gems.refreshGreenGem === true) {
+            gems.greenGem = Math.floor(Math.random() * 13) + 1;
+            gems.refreshGreenGem = false;
+            console.log(gems.greenGem)
+        }
+
+        //this line runs if your score is below the target score
+        if (targetScore > currentScore) {
+            console.log(gems.greenGem);
+            currentScore = currentScore + gems.greenGem;
+            console.log("my target score is: " + targetScore);
+            console.log("my current score is: " + currentScore)
+        }
+        gameState();
     });
+
     $(".purple-gem").on("click", function() {
-        gems.purpleGem = Math.floor(Math.random() * 12) + 1;
-        console.log(gems.purpleGem)
+        if (gems.refreshPurpleGem === true) {
+            gems.purpleGem = Math.floor(Math.random() * 13) + 1;
+            gems.refreshPurpleGem = false;
+            console.log(gems.purpleGem)
+        }
+
+        //this line runs if your score is below the target score
+        if (targetScore > currentScore) {
+            console.log(gems.purpleGem);
+            currentScore = currentScore + gems.purpleGem;
+            console.log("my target score is: " + targetScore);
+            console.log("my current score is: " + currentScore)
+        }
+        gameState();
     });
+
+    console.log(gems.refreshGem)
 
 })
